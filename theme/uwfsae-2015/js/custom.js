7f9caaf675fa3e1d1ffab89fcbd9d6dc6b356ac2
Michael Lee <michael.lee.0x2a@gmail.com>
2016-01-10 17:04:40 -0800

    Fix nav submenu bug

--- theme/uwfsae-2015/js/custom.js
+++ theme/uwfsae-2015/js/custom.js
@@ -3,5 +3,25 @@ $(document).ready(function() {
         $(this).parent().toggleClass('submenu-active');
     }
 
+    function disableSubmenus() {
+        var width = $(this).width();
+        
+        // 768px is from bootstrap's breakpoints.
+        var previousIsSmall = window.currentWidth < 768;
+        var currentIsSmall = width < 768;
+
+        if (previousIsSmall != currentIsSmall) {
+            // Reset menus if we've crossed a breakpoint
+            $('.submenu-toggle').parent().removeClass('submenu-active');
+        }
+        window.currentWidth = width;
+
+    }
+
+    // Global
+    window.currentWidth = $(window).width();
+
+    // Setup event listeners 
     $('.submenu-toggle').on('click', toggleSubmenu);
+    $(window).on('resize', disableSubmenus);
 });
